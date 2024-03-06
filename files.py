8be109383ad42492b6832a54599c3fb245816e97
Ksymena P <ksymenka@duck.com>
2024-03-06 21:25:11 +0100

    created file operations module

---
+++ files.py
@@ -0,0 +1,62 @@
+import os
+import subprocess
+import shutil
+
+class FileOperation:
+    # constuctor methods
+    def __init__(self, source_path : str = os.path.expanduser('~/Pobrane'), desination_path : str = None, changed_files_path : str = os.path.expanduser("~/Movies")):
+        self.source_path = source_path
+        self.desination_path = desination_path or self.check_sus_dest()
+        self.changed_files_path = changed_files_path
+        if not os.path.exists(changed_files_path): 
+            os.makedirs(changed_files_path)
+            
+    def check_sus_dest(self):
+        sus_paths = ["/media/", "/mnt"]
+        for path in sus_paths:
+            if self.check_dest_is_dir(path) is not None:
+                return path
+            
+    def check_dest_is_dir(self, path):
+        if os.path.exists(path):
+            contents = os.listdir(path)
+            for file in contents:
+                file_path = os.path.join(path, file)
+                if os.path.isdir(file_path):
+                    print(file_path, " is a directory")
+                    return os.path.abspath(file_path) 
+        return None
+
+    # file operation methods
+
+    def create_new_name(self, file : str): # used in convert_file
+        path = os.path.abspath(file)
+        name = os.path.splitext(file)
+        new_name = name[0] + '.mp3'
+        return new_name
+    
+    def convert_file(self, file_path : str): # used in process_files
+        new_file_path = os.path.join(os.path.dirname(file_path), self.create_new_name(file_path))
+        subprocess.run(['ffmpeg', '-i', file_path, '-vn', new_file_path, '-loglevel', 'quiet'])
+            
+    def move_files(self, file_path : str): # used to move files to accurate directories
+        if file_path.endswith(".mp3"):
+            print("File", file_path, " is getting moved to ", self.desination_path)
+            shutil.move(file_path, self.desination_path)
+        elif file_path.endswith(".mp4"):
+            print("File", file_path, " is getting moved to ", self.changed_files_path)
+            shutil.move(file_path, self.changed_files_path)            
+        else:
+            print("File", file_path, " is not a music containing file, excluding...")
+
+    def process_files(self): # used to change file format to mp3 from mp4 and then upload it to approprieate 
+        files = os.listdir(self.source_path)
+        for file in files:
+            print("Processing file", file, " atm")
+            file_path = os.path.join(self.source_path, file)
+            self.convert_file(file_path)
+            subprocess.run(["eza", '--tree', file_path])
+        files = os.listdir(self.source_path)
+        for file in files:
+            file_path = os.path.join(self.source_path, file)
+            self.move_files(file_path)
