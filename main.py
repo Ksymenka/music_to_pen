39ac3eefda9f0cda5e8be868079685ed5f2a48d9
Ksymena P <ksymenka@duck.com>
2024-03-18 18:20:20 +0100

    added updating to main.py

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 import sys 
 import os
 sys.path.append(os.path.expanduser("~/.local/lib/python3.11/"))
+sys.path.append(os.path.join(os.getcwd(), "music_to_pen"))
 from music_to_pen.gui import Gui
+from music_to_pen.updater import Updater 
 
 if __name__ == "__main__":
+        update = Updater()
+        update.check_updates()
         gui = Gui(450, 450)
