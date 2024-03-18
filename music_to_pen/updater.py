166753b5208911a6efe703c6d5f43e261baddc7c
Ksymena P <ksymenka@duck.com>
2024-03-18 14:54:45 +0100

    added logic for updater

--- music_to_pen/updater.py
+++ music_to_pen/updater.py
@@ -1,11 +1,14 @@
 import os
 from git import Repo
 import requests
+from install import InstallProject
+from tkinter import messagebox
 
 class Updater:
     def __init__(self) -> None:
         self.project_path = self.get_project_path()
         self.repo = Repo(self.project_path)
+        self.origin = self.repo.remotes.origin 
 
         
     def get_project_path(self) -> None:
@@ -20,19 +23,37 @@ class Updater:
         if response.status_code == 200:
             return response.json()[0]['sha']
 
-    def get_local_commit(self):
+    def get_local_commit(self) -> None:
         return self.repo.head.commit.hexsha
     
-    def check_updates(self):
+    def check_updates(self) -> None:
         if self.get_local_commit() and self.get_remote_commit():
             if self.get_remote_commit() == self.get_local_commit:
                 print("Project is up to date")
             else:
                 print("There is update avaible")
+                self.ask_for_update()
         else:
             print("There was an error while getting commits") 
-    
-     
-
+            
+    def ask_for_update(self) -> None:
+        answer = messagebox.askyesno(title="Update avaible", message="Do you want to update now?")
+        if answer:
+            self.update()
+            
+    def update(self):
+        install = InstallProject()
+        if install.check_if_installed():
+            install.uninstall()
+        try:
+            self.origin.fetch()
+            self.origin.pull()
+            if install.check_if_installed():
+                install.install()
+            
+            print("Updated succesfully")
+        except git.GitCommandError as e:
+            print(f"There was an erroe while updating: {e}")
+        
 update = Updater()
 update.check_updates()
